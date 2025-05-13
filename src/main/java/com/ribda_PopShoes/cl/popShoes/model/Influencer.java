47f0d742122489aee22ba907e6fdf7f471f5a7b1
bnnovdg <bru.rivas@duocuc.cl>
2025-05-13 10:18:08 -0400

    Update Influencer.java

--- src/main/java/com/ribda_PopShoes/cl/popShoes/model/Influencer.java
+++ src/main/java/com/ribda_PopShoes/cl/popShoes/model/Influencer.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -26,4 +28,8 @@ public class Influencer {
 
     @Column(nullable = false)
     private String descripcion;
+
+    @ManyToMany
+    @JoinColumn(name= "id_estilo", nullable = false)
+    private Estilo estilo;
 }

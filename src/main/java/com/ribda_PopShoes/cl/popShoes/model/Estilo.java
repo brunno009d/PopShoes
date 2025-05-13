09db62367cef8d04d406cf41ce989033d7be213d
bnnovdg <bru.rivas@duocuc.cl>
2025-05-13 10:18:48 -0400

    Update Estilo.java

--- src/main/java/com/ribda_PopShoes/cl/popShoes/model/Estilo.java
+++ src/main/java/com/ribda_PopShoes/cl/popShoes/model/Estilo.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,4 +27,12 @@ public class Estilo {
 
     @Column(nullable = false)
     private String descripcion;
+
+    @ManyToMany
+    @JoinColumn(name= "id_influencer", nullable = false)
+    private Influencer influencer;
+
+    @ManyToMany
+    @JoinColumn(name= "id_color", nullable = false)
+    private Color color;
 }

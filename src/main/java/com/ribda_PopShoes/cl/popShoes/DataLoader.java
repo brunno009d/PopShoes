9987514d0a8314a555bf1ace9ca4efa0f560f6df
bnnovdg <bru.rivas@duocuc.cl>
2025-06-15 01:27:21 -0400

    v2.1

--- src/main/java/com/ribda_PopShoes/cl/popShoes/DataLoader.java
+++ src/main/java/com/ribda_PopShoes/cl/popShoes/DataLoader.java
@@ -1,12 +1,20 @@
 package com.ribda_PopShoes.cl.popShoes;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import com.ribda_PopShoes.cl.popShoes.model.Categoria;
+import com.ribda_PopShoes.cl.popShoes.model.Color;
+import com.ribda_PopShoes.cl.popShoes.model.Estilo;
+import com.ribda_PopShoes.cl.popShoes.model.Influencer;
+import com.ribda_PopShoes.cl.popShoes.model.Marca;
 import com.ribda_PopShoes.cl.popShoes.model.Rol;
 import com.ribda_PopShoes.cl.popShoes.repository.CalzadoRepository;
 import com.ribda_PopShoes.cl.popShoes.repository.CategoriaRepository;
@@ -53,9 +61,65 @@ public class DataLoader implements CommandLineRunner{
         for(int i = 0; i < 3; i++){
             Rol rol = new Rol();
             rol.setId(i + 1);
-            
+            rol.setNombre(faker.name().firstName());
+            rolRepository.save(rol);
+        }
+
+        for(int i = 0; i <3; i++){
+            Color color = new Color();
+            color.setId(i + 1);
+            color.setNombre(faker.color().name());
+            colorRepository.save(color);
+        }
+
+        for(int i = 0; i < 3; i ++){
+            Influencer influencer = new Influencer();
+            influencer.setId(i + 1);
+            influencer.setNombre(faker.name().fullName());
+            influencer.setDescripcion(faker.company().catchPhrase());
+            influencerRepository.save(influencer);
+        }
+
+        for(int i = 0; i < 3; i++){
+            Estilo estilo = new Estilo();
+            estilo.setId(i + 1);
+            estilo.setNombre(faker.options().option("Casual", "Informal", "Deportivo"));
+            estilo.setDescripcion(faker.lorem().sentence());
+            estiloRepository.save(estilo);
+        }
+
+        List<Color> colores = colorRepository.findAll();
+        List<Influencer> influencers = influencerRepository.findAll();
+        List<Estilo> estilos = estiloRepository.findAll();
+
+        for(Estilo estilo : estilos){
 
         }
+
+
+        // para estilos color y estilos influencer
+
+
+        for(int i = 0; i < 3; i++){
+            Categoria categoria = new Categoria();
+            categoria.setId(i + 1);
+            categoria.setNombre(faker.commerce().department());
+            categoriaRepository.save(categoria);
+        }
+
+        for(int i = 0; i < 3; i++){
+            Marca marca = new Marca();
+            marca.setId(i + 1);
+            marca.setNombre(faker.company().name());
+            marcaRepository.save(marca);
+        }  
+
+        // calzado
+
+        //usuario
+        for (int i = 0; i < 3; i ++);
+
+
 
     }
 
